7692ee3f14ed894dc29283a106215f80a3b4df9b
aktaruzzamanjoti97 <aktaruzzamanjoti97@gmail.com>
2025-09-28 01:11:57 +0600

    add

--- Array Reduce Transformation/index.js
+++ Array Reduce Transformation/index.js
@@ -1,7 +1,9 @@
 function reduce(nums, fn, init) {
     var res = init;
-    for (var i = 0; i < nums.length; i++) {
-        res = fn(res, nums[i]);
+    for (var _i = 0, nums_1 = nums; _i < nums_1.length; _i++) {
+        var n = nums_1[_i];
+        res = fn(res, n);
     }
     return res;
 }
+;
--- Array Reduce Transformation/index.ts
+++ Array Reduce Transformation/index.ts
@@ -1,9 +1,9 @@
-type Fn = (accum: number, curr: number) => number;
+type Fn = (accum: number, curr: number) => number
 
 function reduce(nums: number[], fn: Fn, init: number): number {
-	let res = init;
-	for (let i = 0; i < nums.length; i++) {
-		res = fn(res, nums[i]);
-	}
-	return res;
-}
+    let res = init;
+    for(const n of nums){
+        res = fn(res, n)
+    }
+    return res;
+};
