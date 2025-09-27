96da6997a0b368f709c89f3a8d61baf892314b04
aktaruzzamanjoti97 <aktaruzzamanjoti97@gmail.com>
2025-09-27 20:38:45 +0600

    fix: added

---
+++ Filter Elements from Array/index.js
@@ -0,0 +1,9 @@
+function filter(arr, fn) {
+    var result = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            result.push(arr[i]);
+        }
+    }
+    return result;
+}
